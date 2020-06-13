0482e492d3c18d5ed5d568131c73df71a22556f3
David Rakosi <david.rakosi99@gmail.com>
2020-06-14 00:46:08 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,45 @@
+
+
+const API_URL = "http://localhost:3000/api/posts";
+const API_BASE_URL = "http://localhost:3000/";
+
+window.onload = () => {
+    getPosts();
+}
+
+const getPosts = () => {
+    fetch(API_URL, {
+        method: 'GET'
+    }).then((res) => {
+        return res.json()
+    }).then((data) => {
+        buildPosts(data)
+    })
+}   
+
+const buildPosts = (blogPosts) => {
+    console.log(blogPosts)
+
+    let blogPostContent = ''
+
+    blogPosts.map((post) => {
+        const postDate = new Date(parseInt(post.added_date)).toDateString()
+        const postImage = `${API_BASE_URL}${post.post_image}`
+        const postLink = `/exercises/post.html?id=${post.id}`
+        blogPostContent += `
+        <a class="post-link" href="${postLink}">
+            <div class="post">
+                <div class="post-image" style="background-image: url(${postImage})"></div>
+                <div class="post-content">
+                    <div class="post-date">${postDate}</div>
+                    <div class="post-title">
+                        <h4>${post.title}</h4>
+                    </div>
+                    <div class="post-text">${post.content}</div>
+                </div>
+            </div>
+        </a>
+        `
+    })
+    document.querySelector('.blog-posts-container').innerHTML = blogPostContent
+}
